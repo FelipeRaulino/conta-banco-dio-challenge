74e5388c35d989a7b42d67f15ae1b9a1efde42fe
Felipe Raulino <felipe.raaulino29@gmail.com>
2024-05-15 00:04:33 -0300

    feat: create project

---
+++ src/ContaTerminal.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        Double saldo;
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu Nome: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o seu Saldo: ");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+    }
+}
